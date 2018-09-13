ef94d931452d41c0c308cc357acc3e409912c8d9
jamesmurphy7 <jamesmurphy@email.arizona.edu>
2018-09-12 19:02:15 -0700

    Add files via upload

---
+++ project2/server.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+int main(int argc, char* argv[]){
+
+	if(argc != 2){
+		fprintf(stderr, "not correct number of commandline args\n");
+	}
+	int portnum;
+	char* badString;
+	portnum = strtol(argv[1], &badString, 0);
+	if(*badString == '\0'){
+		//nothing wrong
+	}
+	else {
+		fprintf(stderr, "bad portnum\n");
+		return 1;
+	}
+
+	//bind socket
+	int sock_desc;
+	sock_desc = socket(PF_INET, SOCK_STREAM, 6);
+	if(sock_desc < 0){
+		fprintf(stderr, "bad socket_desc\n");
+		return 1;
+	}
+	sin.sin_family = PF_INET;
+	//htons(portnum);
+	sin.sin_port = portnum;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	int result = bind(sock_desc, (struct sockaddr *)&sin, sizeof(sin));
+	if (result < 0){
+		fprintf(stderr,"error binding socket\n");
+		return 1;
+	}
+	printf("listening for a connection\n");
+	result = listen(sock_desc, 10);
+	//accept connection here
+	
+
+
+}
