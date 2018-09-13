2e10287571d086294150844e57f2328d888989c0
Kienan6 <kienan6@hotmail.com>
2018-09-12 20:32:35 -0700

    accept set up

--- project2/server.c
+++ project2/server.c
@@ -12,6 +12,38 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+int sock_desc;
+
+int startServer(int portnum) {
+
+	struct sockaddr_in sin;
+	sin.sin_family = PF_INET;
+	//htons(portnum);
+	sin.sin_port = portnum;
+	sin.sin_addr.s_addr = INADDR_ANY;
+
+	int result = bind(sock_desc, (struct sockaddr *)&sin, sizeof(sin));
+
+	if (result < 0){
+		fprintf(stderr,"error binding socket\n");
+		return 1;
+	}
+
+	printf("listening for a connection\n");
+	result = listen(sock_desc, 10);
+	if(result < 0){
+		printf("Error listening to socket.\n");
+		return 1;
+	}
+	//accept a new connection when it is received
+	struct sockaddr_in client;
+	size_t size = sizeof(client);
+	int accepted = accept(sock_desc, (struct sockaddr *) &client, (socklen_t *) &size);
+
+	return 0;
+
+}
+
 int main(int argc, char* argv[]){
 
 	if(argc != 2){
@@ -29,25 +61,12 @@ int main(int argc, char* argv[]){
 	}
 
 	//bind socket
-	int sock_desc;
 	sock_desc = socket(PF_INET, SOCK_STREAM, 6);
 	if(sock_desc < 0){
 		fprintf(stderr, "bad socket_desc\n");
 		return 1;
 	}
-	sin.sin_family = PF_INET;
-	//htons(portnum);
-	sin.sin_port = portnum;
-	sin.sin_addr.s_addr = INADDR_ANY;
-	int result = bind(sock_desc, (struct sockaddr *)&sin, sizeof(sin));
-	if (result < 0){
-		fprintf(stderr,"error binding socket\n");
-		return 1;
-	}
-	printf("listening for a connection\n");
-	result = listen(sock_desc, 10);
-	//accept connection here
-	
 
+	return startServer(portnum);
 
 }
