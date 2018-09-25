f4874dc9cd0d5d761d30ab080c3127d5536f2276
jamesmurphy7 <jamesmurphy@email.arizona.edu>
2018-09-24 17:10:14 -0700

    Add files via upload

--- project2/client.c
+++ project2/client.c
@@ -13,60 +13,9 @@ Authors: Kienan O'Brien, James Murphy
 #include <netinet/in.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]){
-	//error checking
-	if(argc != 3){
-		fprintf(stderr, "not correct number of commandline args\n");
-		return 1;
-	}
-	char* hostName = "";
-	hostName = argv[1];
-	int portnum;
-	char* badString;
-	//get portnum
-	portnum = strtol(argv[2], &badString, 0);
-	if(*badString == '\0'){
-		//printf("port num is: %d\n", portnum);
-	}
-	//error checking
-	else{
-		fprintf(stderr, "bad portnum\n");
-		return 1;
-	}
+int sock_desc;
 
-	//printf("ip is \"%s\"\n",hostName);
-	//printf("port num is \"%d\"\n",portnum);
-
-	//printf("connecting to server\n");
-	//make socket---------------------------------------------------------
-	int sock_desc;
-	sock_desc = socket(PF_INET, SOCK_STREAM, 6);
-
-	struct sockaddr_in sin;
-	sin.sin_family = PF_INET;
-	//copy ip address
-	in_addr_t address;
-	address = inet_addr(hostName);
-	if( address == -1){
-		fprintf(stderr, "error setting ip adress\n");
-		return 1;
-	}
-	memcpy(&sin.sin_addr, &address, sizeof(address));
-	//memcpy (&sin.sin_addr, hptr->h_addr, hptr->h_length); 
-
-	//set portnum------------------------------------------------------------
-	sin.sin_port = htons(portnum);
-
-	//connect ---------------------------------------------------------------
-	int connectError = connect(sock_desc, (struct sockaddr *) &sin, sizeof(sin));
-	if(connectError < 0){
-		fprintf(stderr, "could not connect to %s\n", hostName);
-		return 1;
-	}
-
-	//printf("successfully connected to server\n");
-
-	
+int writeLoop(){
 	//write to server
 	//test print
 	char* toWriteStr = "";
@@ -101,6 +50,78 @@ int main(int argc, char* argv[]){
 			return 1;
 		}
 	}
+	return 0;
+}
+
+int connectToServer(char* hostName, int portnum){
+	struct sockaddr_in sin;
+	sin.sin_family = PF_INET;
+	//copy ip address
+	in_addr_t address;
+	address = inet_addr(hostName);
+	if( address == -1){
+		fprintf(stderr, "error setting ip adress\n");
+		return 1;
+	}
+	memcpy(&sin.sin_addr, &address, sizeof(address));
+	//memcpy (&sin.sin_addr, hptr->h_addr, hptr->h_length); 
+
+	//set portnum------------------------------------------------------------
+	sin.sin_port = htons(portnum);
+
+	//connect ---------------------------------------------------------------
+	int connectError = connect(sock_desc, (struct sockaddr *) &sin, sizeof(sin));
+	if(connectError < 0){
+		fprintf(stderr, "could not connect to %s\n", hostName);
+		return 1;
+	}
+
+	printf("successfully connected to server\n");
 	
 	return 0;
+}
+
+int main(int argc, char* argv[]){
+	//error checking
+	if(argc != 4){
+		fprintf(stderr, "not correct number of commandline args\n");
+		return 1;
+	}
+	char* hostName = "";
+	hostName = argv[2];
+	int portnum;
+	char* badString;
+	//get portnum
+	portnum = strtol(argv[3], &badString, 0);
+	if(*badString == '\0'){
+		//printf("port num is: %d\n", portnum);
+	}
+	//error checking
+	else{
+		fprintf(stderr, "bad portnum\n");
+		return 1;
+	}
+	int portnumTel;
+	portnumTel = strtol(argv[1], &badString, 0);
+	if(*badString == '\0'){
+		//printf("port num is: %d\n", portnum);
+	}
+	//error checking
+	else{
+		fprintf(stderr, "bad portnum\n");
+		return 1;
+	}
+
+	printf("ip is \"%s\"\n",hostName);
+	printf("portnum to server is \"%d\"\n",portnum);
+	printf("portnum to listen is \"%d\"\n",portnumTel);
+
+	printf("connecting to server\n");
+	//make socket---------------------------------------------------------
+	sock_desc = socket(PF_INET, SOCK_STREAM, 6);
+
+	if(connectToServer(hostName, portnum) != 0){
+		return 1;
+	}
+	return writeLoop();
 }
